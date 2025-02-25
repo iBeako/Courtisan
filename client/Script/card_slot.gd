1dd55b81e0b44b18e3f7770cf5c45d7dc92bca7b
LE Duc Bao <bao67200@gmail.com>
2025-02-25 01:28:11 +0100

    Neutral Zone

--- client/Script/card_slot.gd
+++ client/Script/card_slot.gd
@@ -1,28 +1,30 @@
 extends Node2D
+
+# Variables
 var card_in_slot = false
+var cards_in_slot: Array = []  # Liste des cartes dans le slot
+const CARD_SPACING: int = 10  # Espacement entre les cartes
+
+# Énumération pour les types de slot
+enum SlotType { Haut, Bas }
+
+# Propriété exportée pour identifier le type de slot
+@export var slot_type: SlotType = SlotType.Haut
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print($Area2D.collision_mask)
+	print($Area2D.collision_mask)  # Affiche le masque de collision
 
-
-
-var cards_in_slot = []  # Liste des cartes dans le slot
-const CARD_SPACING = 10  # Espacement entre les cartes
-
-func add_card(card):
+# Ajouter une carte au slot
+func add_card(card: Node2D) -> void:
 	if card in cards_in_slot:
 		return  # Évite d'ajouter la même carte plusieurs fois
 
-	# Récupérer la largeur de l'écran
-	var screen_width = get_viewport_rect().size.x
-
 	# Déterminer la position en fonction du type de carte
-	var target_x_position = calculate_card_position_based_on_type(card.card_type)
-	var target_position = Vector2(target_x_position, self.position.y)
+	var target_position: Vector2 = calculate_card_position_based_on_type(card.card_type)
 
 	# Animation pour un placement fluide
-	var tween = get_tree().create_tween()
+	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(card, "position", target_position, 0.2)
 
 	# Désactiver la collision pour éviter les conflits
@@ -31,33 +33,57 @@ func add_card(card):
 	# Ajouter la carte à la liste des cartes dans le slot
 	cards_in_slot.append(card)
 
-func calculate_card_position_based_on_type(card_type):
+# Calculer la position de la carte en fonction de son type
+func calculate_card_position_based_on_type(card_type: String) -> Vector2:
+	print("Calculating position for card type: ", card_type)
+
+	var node_name: String = ""
+
+	# Associer chaque type de carte à son slot haut ou bas
 	match card_type:
 		"blanc":
-			return 344  # Position en pixels
+			node_name = "Blanc_" + SlotType.keys()[slot_type]
 		"vert":
-			return 550  # Position en pixels
+			node_name = "Vert_" + SlotType.keys()[slot_type]
 		"rouge":
-			return 760  # Position en pixels
+			node_name = "Rouge_" + SlotType.keys()[slot_type]
 		"jaune":
-			return 1160  # Position en pixels
+			node_name = "Jaune_" + SlotType.keys()[slot_type]
 		"marron":
-			return 1340  # Position en pixels
+			node_name = "Marron_" + SlotType.keys()[slot_type]
 		"bleu":
-			return 1550  # Position en pixels
+			node_name = "Bleu_" + SlotType.keys()[slot_type]
 		_:
-			return 324  # Par défaut, positionner à 324 px
+			print("Unknown card type: ", card_type)
+			return Vector2(0, 0)
 
-func update_card_positions():
-	for i in range(cards_in_slot.size()):
-		var card = cards_in_slot[i]
+	# Trouver le slot dans NeutralZone
+	var neutral_zone: Node = get_node("/root/Main/NeutralZone")
+	if not neutral_zone:
+		print("Error: NeutralZone not found!")
+		return Vector2(0, 0)
+
+	var slot_node: Node2D = neutral_zone.get_node(node_name)
+	if slot_node:
+		print("Node found: ", slot_node.name, " at position ", slot_node.position)
+		return slot_node.position
+	else:
+		print("Node not found: ", node_name)
+		return Vector2(0, 0)
+
+# Mettre à jour les positions des cartes
+func update_card_positions() -> void:
+	for i: int in range(cards_in_slot.size()):
+		var card: Node2D = cards_in_slot[i]
 		card.position = position + Vector2(i * CARD_SPACING, 0)  # Décalage horizontal
 		card.get_node("Area2D/CollisionShape2D").disabled = true  # Désactiver la collision
 
-func remove_card(card):
+# Retirer une carte du slot
+func remove_card(card: Node2D) -> void:
 	if card in cards_in_slot:
 		cards_in_slot.erase(card)
 		update_card_positions()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
