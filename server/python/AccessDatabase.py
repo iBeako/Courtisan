f1cd1cd7f2a8033353ec09246b993feba0a66e5f
Remi Verdun <remi.verdun@etu.unistra.fr>
2025-03-31 22:33:13 +0200

    test for database

--- server/python/AccessDatabase.py
+++ server/python/AccessDatabase.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, WebSocket, Depends
 import cx_Oracle
 from sshtunnel import SSHTunnelForwarder
 from contextlib import contextmanager
+import os
 
 app = FastAPI()
 
